ec93f6d9276dcc8ea0acf5e2c0d67161c5799859
Morgan Fell <morgan@morganfell.dev>
2022-03-08 23:06:05 +0000

    Slightly reformatted text.

--- novabot-core/novabot_core.py
+++ novabot-core/novabot_core.py
@@ -5,20 +5,19 @@ from discord.ext import commands
 from discord.ext.commands import Bot
 from discord.ext import tasks
 
-TOKEN = os.getenv('discord_token')
-resources_location = os.getenv('novabot_resources')
+TOKEN = os.getenv("discord_token")
+resources_location = os.getenv("novabot_resources")
 
 # 'nd!' is the DEV prefix for NovaBot
 bot = Bot(command_prefix="nd!")
 
 @bot.event
 async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
+    print(f"{bot.user.name} has connected to Discord!")
     print("Resources Location: " + str(resources_location))
 
 @bot.command()
 async def hello(ctx):
-    await ctx.send('Hello, world!')
-    
+    await ctx.send("Hello, world!")
 
 bot.run(TOKEN)
