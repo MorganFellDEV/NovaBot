0c89e9593f87e9ea7d2cbc6a2b65daf229c4879d
Morgan Fell <morgan@morganfell.dev>
2022-03-12 23:03:24 +0000

    Added n!textnova Uses the Twilio Python library to send SMS messages to Nova's personal phone. This will definitely annoy him.

--- novabot-core/chat_commands/cmd_cog.py
+++ novabot-core/chat_commands/cmd_cog.py
@@ -1,16 +1,25 @@
 import discord
 import discord.emoji
 import requests
+import os
 import sys
 from discord.ext import commands
 from discord.ext.commands import Bot
 from discord.ext import tasks
 from chat_commands import cmd_actions
 from chat_commands import get_emote
+from twilio.rest import Client
 
 from chat_commands.FileStore import FileStore
 
 fileStore = FileStore()
+
+twilio_account_sid = os.getenv("twilio_sid")
+twilio_account_token = os.getenv("twilio_token")
+nova_phone_number = os.getenv("nova_phone_number")
+novabot_prefix = os.getenv("novabot_prefix")
+
+twilioclient = Client(twilio_account_sid, twilio_account_token)
 
 class BotCommands(commands.Cog):
     def __init__(self, bot):
@@ -55,6 +64,32 @@ class BotCommands(commands.Cog):
     async def jumbo(self,ctx, emoji: discord.PartialEmoji):
         try:
             await ctx.send(file=get_emote.get_emote_image(ctx, emoji, fileStore))
+        except:
+            print(sys.exc_info())
+
+    @commands.command()
+    @commands.cooldown(1, 60, commands.BucketType.guild)
+    async def textnova(self,ctx):
+        try:
+            user_message_string = ctx.message.content
+            user_message_string = user_message_string.replace(str(novabot_prefix+"annoynova "),"")
+            print (user_message_string)
+            if (len(user_message_string) < 100):
+                twilioclient.api.account.messages.create(
+                    to=nova_phone_number,
+                    from_="NovaBot",
+                    body=str(ctx.message.author) + ' says ' + '"' + user_message_string + '"')
+                await ctx.send("Text message sent to Nova!")
+            else:
+                await ctx.send("Text message too long!")
+        except:
+            print(sys.exc_info())
+
+    @commands.command()
+    async def repeat(self,ctx):
+        try:
+            
+            await ctx.send(user_message_string)
         except:
             print(sys.exc_info())
 
